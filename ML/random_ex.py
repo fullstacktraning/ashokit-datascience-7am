2f4876ab0c2bcd130d2e8a82573bcafcf8869512
Samba <samba.pendela@gmail.com>
2026-03-13 07:48:37 +0530

    code

---
+++ ML/random_ex.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+
+data = {
+    "Outlook": ["Sunny","Sunny","Overcast","Rain","Rain","Rain","Overcast","Sunny"],
+    "Temperature": ["Hot","Hot","Hot","Mild","Cool","Cool","Mild","Hot"],
+    "Humidity": ["High","High","High","High","Normal","Normal","Normal","High"],
+    "Play": ["No","No","Yes","Yes","Yes","No","Yes","No"]
+}
+
+df = pd.DataFrame(data)
+
+encoder = LabelEncoder()
+
+df["Outlook"] = encoder.fit_transform(df["Outlook"])
+df["Temperature"] = encoder.fit_transform(df["Temperature"])
+df["Humidity"] = encoder.fit_transform(df["Humidity"])
+df["Play"] = encoder.fit_transform(df["Play"])
+
+X = df[["Outlook","Temperature","Humidity"]]
+y = df["Play"]
+
+model = RandomForestClassifier(n_estimators=10,criterion="entropy")
+
+model.fit(X,y)
+
+
+res = model.predict([[1,0,1]])
+
+
+print(res[0])
+
+# Sunny -- 2. Rain -- 1. Overcast -- 0
+# Cool -- 0 Hot -- 1.  Mild -- 2
+# High -- 0  Normal -- 1
