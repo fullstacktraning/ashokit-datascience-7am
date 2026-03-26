9c1a2d1a7df3ad01295fd4aee5b3cf5a451c7248
AshokIT <info@ashokit.in>
2026-03-26 07:52:05 +0530

    code

--- ML/unsupervised/kmeans_ex.py
+++ ML/unsupervised/kmeans_ex.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 X = np.array([[1,2],[2,3],[3,4],[8,8],[9,9],[10,10]])
 model = KMeans(n_clusters=2)
 model.fit(X)
-labels = model.labels_
+labels = model.labels_ # 000  111
 plt.scatter(X[:,0],X[:,1],c=labels)
 plt.show()
 print(labels)
+
+# Elbow method  Sihouette Method
+
+# Elbow method (WCSS) (Within Cluster Square od sums)
