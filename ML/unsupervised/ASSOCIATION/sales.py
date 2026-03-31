1de6dfb70338d005e9b724b3e20fcba4689223dc
AshokIT <info@ashokit.in>
2026-03-31 07:26:00 +0530

    code

---
+++ ML/unsupervised/ASSOCIATION/sales.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# Step 1: Dataset
+transactions = [
+    ['Milk', 'Bread'],
+    ['Milk', 'Butter'],
+    ['Bread', 'Butter'],
+    ['Milk', 'Bread'],
+    ['Milk', 'Bread', 'Butter']
+]
+
+# Step 2: Convert to DataFrame
+te = TransactionEncoder()
+te_data = te.fit(transactions).transform(transactions)
+df = pd.DataFrame(te_data, columns=te.columns_)
+
+print("Dataset:\n", df)
+
+# Step 3: Apply Apriori
+frequent_items = apriori(df, min_support=0.6, use_colnames=True)
+print("\nFrequent Itemsets:\n", frequent_items)
+
+# Step 4: Generate Rules
+rules = association_rules(frequent_items, metric="confidence", min_threshold=0.7)
+
+print("\nAssociation Rules:\n", rules)
